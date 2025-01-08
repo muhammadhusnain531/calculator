f6f462f1c92ee02e39247daaa6c74fb73b1fe52a
Muhammad Husnain <muhammadhusnain472633@gmail.com>
2025-01-08 20:16:40 +0500

    code

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var userinput = '';
+  var answer = '';
+  final List<String> buttons = [
+   ' C',
+    '+/-',
+    '%',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    'x',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,12 +62,59 @@ class _HomeScreenState extends State<HomeScreen> {
                Container(
                  padding: EdgeInsets.all(20),
                  alignment: Alignment.centerRight,
-                 child: Text('Number'),
-               )
+                 child: Text("userinput", //input user
+                   style: TextStyle(fontSize: 18,color: Colors.black87),),
+               ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  alignment: Alignment.centerRight,
+                  child: Text("Answer",style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black87,
+                  fontWeight: FontWeight.bold),),
+                ),
+                Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4
+                      ), itemCount: buttons.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        
+
+                      },),
+
+                ))
               ],
             ),
           ))
         ],
+      ),
+    );
+  }
+}
+// Button
+class MyButton extends StatelessWidget {
+final color;
+final textColor;
+final String buttonText;
+final buttontapped;
+MyButton({this.buttontapped,required this.buttonText,this.color,this.textColor});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttontapped,
+      child: ClipRRect(
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              buttonText,style: TextStyle(color: color,
+            fontSize: 25,
+            fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
       ),
     );
   }
